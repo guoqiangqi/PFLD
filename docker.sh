088ed8b4b1ee403fde4e8009018ea1c622d58b2b
guoqiangqi <425418567@qq.com>
2019-05-27 20:21:42 +0800

    update docker .sh file

---
+++ docker.sh
@@ -0,0 +1,1 @@
+nvidia-docker run -ti -v /home-ex/tclhk/guoqiang/:/root -v /home-ex/tclhk/guoqiang/:/home-ex/tclhk/guoqiang/ --privileged=true  nvidia/cuda:8.0-cudnn6-devel-ubuntu16.04 /bin/bash
---
+++ docker_run.sh
@@ -0,0 +1,2 @@
+nvidia-docker start 1d4438d30f09
+nvidia-docker exec -it 1d4438d30f09 /bin/bash
